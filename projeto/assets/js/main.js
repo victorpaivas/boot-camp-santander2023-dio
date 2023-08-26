7b908691085e95b84134502dbd7e1b643e9ed497
Victor Paiva <victorrdps7@gmail.com>
2023-08-25 23:36:46 -0300

    projeto pokedex dio

--- projeto/assets/js/main.js
+++ projeto/assets/js/main.js
@@ -1,8 +1,34 @@
-const offset = 0
-const limit = 5
 
-const url = `https://pokeapi.co/api/v2/pokemon?${offset}&${limit}`
+// Convertendo para Lista Html
+function convertPokemonToLi(pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
 
-fetch(url).then(response => {
-    console.log(response)
-})
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">paison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+    `
+}
+
+const pokemonList =  document.querySelector('#pokemonList');
+
+// Consumo de API
+
+   pokeApi.getPokemons().then(pokemons => {
+        const listItems = []
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            listItems.push(convertPokemonToLi(pokemon))
+        }
+
+        console.log(listItems)
+    })
