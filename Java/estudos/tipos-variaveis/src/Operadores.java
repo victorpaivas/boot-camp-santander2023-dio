770146aa8a533318ea9d67389a7d2d8ae4c789a7
Victor Paiva <victorrdps7@gmail.com>
2023-10-04 13:49:20 -0300

    Java estudos

---
+++ Java/estudos/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 7;
+        b = 6;
+       
+    }
+}
---
+++ Java/estudos/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        // byte idade 123;
+        // short ano = 2023;   
+        // int cep = 11443234; // se começar com zero, talvez tenha que ser outro tipo
+        // long cpf = 29123313600L; // se começar com zero, talvez tenha que ser outro tipo "string" e long precisa ter L no final
+        // float pi = 3.14F; // precisar sempre ter o F no final do tipo float
+        // double salario = 1275.33;
+    }
+}
